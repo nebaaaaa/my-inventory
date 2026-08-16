b231d2d7a3cdedbbd08d710183a146db19944e20
nebaaaaa <nebiyueliasss@gmail.com>
2026-08-16 18:43:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 // keep seeing the old app until the cache naturally expires.
 // =====================================================================
 
-const CACHE_NAME = 'neba-erp-shell-v10';
+const CACHE_NAME = 'neba-erp-shell-v11';
 
 const ASSETS_TO_CACHE = [
   './',
